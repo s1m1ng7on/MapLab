b64c39ce998c4e153152e2bbbf91e803dc1e8614
Simeon Gunev <simganev@gmail.com>
2025-02-13 14:09:29 +0200

    Periodic commit (13/02/2025)

--- MapLab.Web/Program.cs
+++ MapLab.Web/Program.cs
@@ -135,9 +135,16 @@ namespace MapLab
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapAreaControllerRoute(
+                name: "admin",
+                areaName: "Admin",
+                pattern: "Admin/{controller=Dashboard}/{action=Index}"
+            );
+
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
 
             app.MapRazorPages();
             app.MapBlazorHub();
